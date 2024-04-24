5b25b2e3465f728386df715e2227b503a012a285
lirubao <lirubaogo@gmail.com>
2024-04-24 13:51:42 +0800

    feat: Fetch API

--- pages/products/[id].vue
+++ pages/products/[id].vue
@@ -1,12 +1,17 @@
 <template>
     <div>
-        <p>Products details for {{ id }}</p>
+        <p>{{ product.title }}</p>
+        <p>{{ product.price }}</p>
+        <p>{{ product.description }}</p>
+        <img :src="`${product.image}`" alt="">
     </div>
 </template>
 
-<script setup>
+<script setup lang="ts">
 const { id } = useRoute().params
+const uri = "https://fakestoreapi.com/products/" + id
 
+const { data: product } = await useFetch(uri)
 definePageMeta({
     layout: "products"
 })
