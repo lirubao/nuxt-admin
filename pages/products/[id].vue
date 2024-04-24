02c6a9e2d56cac0171d88440a3b941db86ded412
lirubao <lirubaogo@gmail.com>
2024-04-24 17:39:25 +0800

    Global error page definition

--- pages/products/[id].vue
+++ pages/products/[id].vue
@@ -9,6 +9,10 @@ const { id } = useRoute().params
 const uri = "https://fakestoreapi.com/products/" + id
 
 const { data: product } = await useFetch(uri)
+
+if (!product.value) {
+    throw createError({ statusCode: 404, statusMessage: 'Product not found', fatal: true })
+}
 definePageMeta({
     layout: "products"
 })
