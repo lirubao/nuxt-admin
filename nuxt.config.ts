98315de908220271078e575b91d625c8b83bbb1d
lirubao <lirubaogo@gmail.com>
2024-04-24 18:59:05 +0800

    feat: Global SEO

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,17 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
+  app: {
+    head: {
+      title: "Nuxt Dojo",
+      meta: [
+        { name: "description", content: "Everyhing about Nuxt 3" }
+      ],
+      link: [
+        { rel: "stylesheet", href: "https://fonts.loli.net/icon?family=Material+Icons" }
+      ]
+    }
+  },
   modules: [
     '@nuxtjs/tailwindcss'
   ]
